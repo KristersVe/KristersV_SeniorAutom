8b24c315efac29f046ff670bae02ad5115d67a31
Kristers Vēveris <kristers.veveris@testdevlab.com>
2024-03-04 05:42:27 +0200

    update conftest with page drivers

--- conftest.py
+++ conftest.py
@@ -1,15 +1,20 @@
-import time
+import os
 import pytest
-from appium import webdriver as appium_webdriver
-from appium.webdriver.appium_service import AppiumService
-from appium.options.android import UiAutomator2Options
 from selenium import webdriver as selenium_webdriver
-from appium_setup import AppiumDriverSetup
 from selenium.webdriver.firefox.options import Options
+from pages.github import GitHubPage
+from pages.jitsi import JitsiPage
+from pages.conference import ConferencePage
+from appium_setup import start_appium_service, stop_appium_service, install_and_setup_app, create_android_driver
 
 
 @pytest.fixture
 def chrome_driver():
+    # Could be used for headless mode
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # driver = selenium_webdriver.Chrome(options=chrome_options)
+
     driver = selenium_webdriver.Chrome()
     driver.maximize_window()
     driver.implicitly_wait(10)
@@ -24,17 +29,31 @@ def firefox_driver():
 
     driver = selenium_webdriver.Firefox(options=options)
     driver.maximize_window()
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
 
 @pytest.fixture
-def setup_appium():
-    appdriver = AppiumDriverSetup()
-    yield appdriver
-    appdriver.quit_driver()
+def appium_driver_conf():
+    desired_caps = {
+        'platformName': 'Android',
+        'deviceName': 'R5CT40ZASMM',
+        'automationName': 'UiAutomator2',
+        'appPackage': 'com.ionicframework.conferenceapp',
+        'appActivity': 'MainActivity',
+        'disableIdLocatorAutocompletion': True,
+        'app': os.path.abspath('/Users/kristersveveris/Downloads/conference-app-android.apk')
+    }
 
+    appium_service = start_appium_service()
+    install_and_setup_app(desired_caps['app'])
+    driver = create_android_driver(desired_caps)
+
+    yield driver
+
+    driver.quit()
+    stop_appium_service(appium_service)
 
 @pytest.fixture
 def appium_driver_jitsi():
@@ -48,18 +67,28 @@ def appium_driver_jitsi():
         'disableIdLocatorAutocompletion': True
     }
 
-    appium_service = AppiumService()
-    appium_service.start()
-
-    capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
-
-    driver = appium_webdriver.Remote(command_executor='http://localhost:4723', options=capabilities_options)
-    time.sleep(10)
+    appium_service = start_appium_service()
+    driver = create_android_driver(desired_caps)
 
     yield driver
 
     driver.quit()
-    appium_service.stop()
+    stop_appium_service(appium_service)
+
+
+@pytest.fixture
+def github_page(chrome_driver):
+    yield GitHubPage(chrome_driver)
+
+
+@pytest.fixture
+def conference_page(appium_driver_conf):
+    yield ConferencePage(appium_driver_conf)
+
+
+@pytest.fixture
+def jitsi_page(firefox_driver, appium_driver_jitsi):
+    yield JitsiPage(firefox_driver, appium_driver_jitsi)
 
 
 # This hook could have been used for screenshot captures

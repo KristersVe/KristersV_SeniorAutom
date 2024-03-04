955db4848e3a6dc870e7a7e31de158adac67af6c
Kristers Vēveris <kristers.veveris@testdevlab.com>
2024-03-04 05:43:03 +0200

    refactor the class to contain appium helper functions

--- appium_setup.py
+++ appium_setup.py
@@ -1,40 +1,26 @@
 import os
-import time
-from appium import webdriver
 from appium.webdriver.appium_service import AppiumService
 from appium.options.android import UiAutomator2Options
+from appium import webdriver as appium_webdriver
 
 
-def reinstall_app():
-    """App reinstall"""
+def start_appium_service():
+    appium_service = AppiumService()
+    appium_service.start()
+    return appium_service
+
+
+def stop_appium_service(appium_service):
+    appium_service.stop()
+
+
+def install_and_setup_app(package_path):
     os.system("adb uninstall com.ionicframework.conferenceapp")
-    os.system("adb install /Users/kristersveveris/Downloads/conference-app-android.apk")
+    os.system(f"adb install {package_path}")
 
 
-class AppiumDriverSetup:
-    """Class for appium driver setup"""
-
-    def __init__(self):
-        self.desired_caps = {
-            'platformName': 'Android',
-            'deviceName': 'R5CT40ZASMM',
-            'automationName': 'UiAutomator2',
-            'appPackage': 'com.ionicframework.conferenceapp',
-            'disableIdLocatorAutocompletion': True,
-            'app': os.path.abspath('/Users/kristersveveris/Downloads/conference-app-android.apk'),
-        }
-
-        self.appium_service = AppiumService()
-        self.appium_service.start()
-
-        reinstall_app()
-
-        self.capabilities_options = UiAutomator2Options().load_capabilities(self.desired_caps)
-        self.driver = webdriver.Remote(command_executor='http://localhost:4723', options=self.capabilities_options)
-
-        time.sleep(10)
-
-    def quit_driver(self):
-        """Quit driver"""
-        self.driver.quit()
-        self.appium_service.stop()
+def create_android_driver(capabilities):
+    capabilities_options = UiAutomator2Options().load_capabilities(capabilities)
+    driver = appium_webdriver.Remote(command_executor='http://localhost:4723', options=capabilities_options)
+    driver.implicitly_wait(10)
+    return driver
